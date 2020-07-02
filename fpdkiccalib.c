4eef417d74a0af26f246027f9cd730d5c7e0b6fa
freepdk <free-pdk@users.noreply.github.com>
2020-07-02 09:42:27 +0200

    update year in header

--- fpdkiccalib.c
+++ fpdkiccalib.c
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2019  freepdk  https://free-pdk.github.io
+Copyright (C) 2019-2020  freepdk  https://free-pdk.github.io
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
